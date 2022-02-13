92cb6a7a35d3b00d7431f3af3a5d50d3395dcbf2
David Razmadze <razd45@gmail.com>
2022-02-13 13:51:02 -0500

    Complete tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+// Import ABI Code to interact with smart contract
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import "./App.css";
+
+// The contract address
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  // Property Variables
+
+  const [message, setMessage] = useState("");
+  const [currentGreeting, setCurrentGreeting] = useState("");
+
+  // Helper Functions
+
+  // Requests access to the user's Meta Mask Account
+  // https://metamask.io/
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  // Fetches the current value store in greeting
+  async function fetchGreeting() {
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        // Call Greeter.greet() and display current greeting in `console`
+        /* 
+          function greet() public view returns (string memory) {
+            return greeting;
+          }
+        */
+        const data = await contract.greet();
+        console.log("data: ", data);
+        setCurrentGreeting(data);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    }
+  }
+
+  // Sets the greeting from input text box
+  async function setGreeting() {
+    if (!message) return;
+
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      // Create contract with signer
+      /*
+        function setGreeting(string memory _greeting) public {
+          console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+          greeting = _greeting;
+        } 
+      */
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(message);
+
+      setMessage("");
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
+  // Return
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        {/* DESCRIPTION  */}
+        <div className="description">
+          <h1>Greeter.sol</h1>
+          <h3>Full stack dapp using ReactJS and Hardhat</h3>
+        </div>
+        {/* BUTTONS - Fetch and Set */}
+        <div className="custom-buttons">
+          <button onClick={fetchGreeting} style={{ backgroundColor: "green" }}>
+            Fetch Greeting
+          </button>
+          <button onClick={setGreeting} style={{ backgroundColor: "red" }}>
+            Set Greeting
+          </button>
+        </div>
+        {/* INPUT TEXT - String  */}
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder="Set Greeting Message"
+        />
+
+        {/* Current Value stored on Blockchain */}
+        <h2 className="greeting">Greeting: {currentGreeting}</h2>
+      </div>
     </div>
   );
 }
